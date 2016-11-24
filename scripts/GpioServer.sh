1a430d07bcb0a92411b0110b5abd8dddede8b8db
alexg <alejandrogomezpublic@gmail.com>
2016-11-24 15:28:46 +0000

    GPIO_server script bash to check the GPIO 7 and 17 status

--- scripts/GpioServer.sh
+++ scripts/GpioServer.sh
@@ -16,22 +16,22 @@ while :
 do
 #Read MySQL Data
 #Direction
-direction17=$(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword gpio -e "SELECT status FROM irrigation WHERE pinNumber='17'";)
-direction7=$(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword gpio -e "SELECT status FROM irrigation WHERE pinNumber='7'";)
+direction17=$(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword datalogger -e "SELECT status FROM irrigation WHERE pinNumber='17'";)
+direction7=$(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword datalogger -e "SELECT status FROM irrigation WHERE pinNumber='7'";)
 #Status
-status17=$(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword gpio -e "SELECT status FROM irrigation WHERE pinNumber='17'";)
-status7=$(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword gpio -e "SELECT status FROM irrigation WHERE pinNumber='7'";)
+status17=$(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword datalogger -e "SELECT status FROM irrigation WHERE pinNumber='17'";)
+status7=$(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword datalogger -e "SELECT status FROM irrigation WHERE pinNumber='7'";)
 
 #Run Commands
 if [ "$direction17" == "out" ]; then
     echo "out" > /sys/class/gpio/gpio17/direction
     if [ "$status17" == "1" ]; then
         echo "1" > /sys/class/gpio/gpio17/value
-        $(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword sensor -e "INSERT INTO `irrigation` (`ttime`, `status`, `onoff`, `pinNumber`) VALUES (NOW(), '1', 'ON', '17')";)
+        $(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword datalogger -e "INSERT INTO `irrigation` (`ttime`, `status`, `onoff`, `pinNumber`) VALUES (NOW(), '1', 'ON', '17')";)
         echo "GPIO 17 Turned On"
     else
         echo "0" > /sys/class/gpio/gpio17/value
-        $(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword sensor -e "INSERT INTO `irrigation` (`ttime`, `status`, `onoff`, `pinNumber`) VALUES (NOW(), '0', 'OFF', '17')";)
+        $(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword datalogger -e "INSERT INTO `irrigation` (`ttime`, `status`, `onoff`, `pinNumber`) VALUES (NOW(), '0', 'OFF', '17')";)
         echo "GPIO 17 Turned Off"
     fi
 else
@@ -41,11 +41,11 @@ if [ "$direction7" == "out" ]; then
         echo "out" > /sys/class/gpio/gpio7/direction
     if [ "$status7" == "1" ]; then
                 echo "1" > /sys/class/gpio/gpio7/value
-                $(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword sensor -e "INSERT INTO `irrigation` (`ttime`, `status`, `onoff`, `pinNumber`) VALUES (NOW(), '1', 'ON', '7')";)
+                $(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword datalogger -e "INSERT INTO `irrigation` (`ttime`, `status`, `onoff`, `pinNumber`) VALUES (NOW(), '1', 'ON', '7')";)
                 echo "GPIO 7 Turned On"
         else
                 echo "0" > /sys/class/gpio/gpio7/value
-                $(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword sensor -e "INSERT INTO `irrigation` (`ttime`, `status`, `onoff`, `pinNumber`) VALUES (NOW(), '0', 'OFF', '7')";)
+                $(mysql -B --disable-column-names --user=$mysqlusername --password=$mysqlpassword datalogger -e "INSERT INTO `irrigation` (`ttime`, `status`, `onoff`, `pinNumber`) VALUES (NOW(), '0', 'OFF', '7')";)
                 echo "GPIO 7 Turned Off"
         fi
 else
